c7d56a396bfac97bbe80c6d3d6bce534ee5bbaac
chopperkuang <chopperkuang@gmail.com>
2015-12-07 16:29:36 +0800

    先提交一个版本看看。 所有对字符的操作，都会去掉前后空格，合适么？

--- src/main/java/com/dooioo/td/utils/Is.java
+++ src/main/java/com/dooioo/td/utils/Is.java
@@ -2,24 +2,29 @@ package com.dooioo.td.utils;
 
 import com.sun.istack.internal.NotNull;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import static org.apache.commons.lang3.StringUtils.isEmpty;
-import static org.apache.commons.lang3.StringUtils.trim;
+import static org.apache.commons.lang3.StringUtils.stripToEmpty;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.Objects;
 import java.util.regex.Pattern;
 
 /**
  * Micro check tools
  *
+ * 所有对字符的操作，都会去掉前后空格
+ *
  * @author 闷骚乔巴
  * @since 15/12/3
  */
 public final class Is  {
 
-    //start String.
+    //start empty.
     /**
      * 字符串为空的检查
      * empty ("") or null.
@@ -36,7 +41,7 @@ public final class Is  {
      * @return
      */
     public static boolean empty(final String s) {
-        return isEmpty(trim(s));
+        return isEmpty(stripToEmpty(s));
     }
 
     /**
@@ -54,21 +59,47 @@ public final class Is  {
      * @return
      */
     public static boolean empty(final Object o) {
-        return o == null || empty(o.toString());
+        if(o == null) {
+            return true;
+        }
+
+        if(o instanceof CharSequence) {
+            return isEmpty((CharSequence) o);
+        }
+
+        if (o instanceof Collection) {
+            return CollectionUtils.isEmpty((Collection<?>) o);
+        }
+
+        if (o instanceof Map) {
+            return MapUtils.isEmpty((Map<?, ?>) o);
+        }
+
+        if (o instanceof Object[]) {
+            return ArrayUtils.isEmpty((Object[]) o);
+        }
+
+        return false;
+
     }
 
-    public static boolean empty(final String [] strings) {
-        return ArrayUtils.isEmpty(strings);
+    //~end empty.
+
+
+    public static boolean include(final String str, final String searchStr) {
+        return StringUtils.contains(stripToEmpty(str), stripToEmpty(searchStr));
     }
 
-    //~end String.
+    public static boolean startWith(String str, String prefix) {
+        return StringUtils.startsWith(stripToEmpty(str), stripToEmpty(prefix));
+    }
 
+    public static boolean endsWith(String str, String prefix) {
+        return StringUtils.endsWith(stripToEmpty(str), stripToEmpty(prefix));
+    }
 
     //===========================================================================
     //start Object.
-    public static boolean empty(Object [] objects) {
-        return ArrayUtils.isEmpty(objects);
-    }
 
     /**
      * 是否相等(Null-safe)
@@ -94,14 +125,6 @@ public final class Is  {
 
     //~end Object.
 
-    //===========================================================================
-    //start array
-    public static boolean empty(Collection<?> coll) {
-        return CollectionUtils.isEmpty(coll);
-
-    }
-
-    //~end array
 
     //start RegExp.
     /**
@@ -113,33 +136,69 @@ public final class Is  {
     public static boolean email(@NotNull final String s) {
         if(empty(s)) return false;
 
-        return EMAIL.matcher(s).find();
+        return EMAIL.matcher(stripToEmpty(s)).matches();
     }
 
     public static boolean url(@NotNull final String s) {
         if(empty(s)) return false;
 
-        return URL.matcher(s).find();
+        return URL.matcher(stripToEmpty(s)).matches();
+    }
+
+    /**
+     * 简单验证手机号码
+     * <pre>
+     * Assert.assertTrue(Is.mobile("15612341234"));
+     * Assert.assertTrue(Is.mobile("13512341234"));
+     * Assert.assertTrue(Is.mobile("013512341234"));
+     * Assert.assertTrue(Is.mobile("0156 1234 1234"));
+     * Assert.assertTrue(Is.mobile("0 1560 1234 811"));
+     * Assert.assertTrue(Is.mobile("08615612341234"));
+     * Assert.assertTrue(Is.mobile("8615612341234"));
+     * Assert.assertTrue(Is.mobile("+8615612341234"));
+     * </pre>
+     * @param s
+     * @return
+     */
+    public static boolean mobile(@NotNull final String s) {
+        if(empty(s)) return false;
+
+        String cleanMoibleStr = s.replaceAll("([\\s\\t])|(^086|86)|", "");
+        cleanMoibleStr = cleanMoibleStr.replaceAll("^\\+", "");
+        return MOBILE.matcher(stripToEmpty(cleanMoibleStr)).matches();
+    }
+
+    /**
+     * 验证ip
+     * 暂只支持ipv4
+     *
+     * @param s
+     * @return
+     */
+    public static boolean ip(@NotNull final String s) {
+        if(empty(s)) return false;
+
+        return ip4(stripToEmpty(s));
+    }
+
+    private static boolean ip4(@NotNull final String s) {
+        if(empty(s)) return false;
+
+        return IP4.matcher(stripToEmpty(s)).matches();
+    }
+
+    //暂无需求，不支持
+    private static boolean ip6(@NotNull final String s) {
+        if(empty(s)) return false;
+
+        return IP6.matcher(stripToEmpty(s)).matches();
     }
     //~end RegExp.
 
+    //RegExp
     private static final Pattern EMAIL = Pattern.compile("^((([a-z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])+(\\.([a-z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])+)*)|((\\x22)((((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(([\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x7f]|\\x21|[\\x23-\\x5b]|[\\x5d-\\x7e]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(\\\\([\\x01-\\x09\\x0b\\x0c\\x0d-\\x7f]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF]))))*(((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(\\x22)))@((([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.)+(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))$", Pattern.CASE_INSENSITIVE);
     private static final Pattern URL = Pattern.compile("^(?:(?:https?|ftp):\\/\\/)?(?:(?!(?:10|127)(?:\\.\\d{1,3}){3})(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))(?::\\d{2,5})?(?:\\/\\S*)?$", Pattern.CASE_INSENSITIVE);
-
-    public static void main(String[] args) {
-        Object o = null;
-
-        System.out.println(Is.empty(""));     //true
-        System.out.println(Is.empty(o));      //true
-
-        System.out.println("email ==> ");
-        System.out.println(Is.email("ch"));  //false
-        System.out.println(Is.email("Chopperkuang@gmail.com"));  //true
-        System.out.println(Is.email("中国@gmail.com")); //true
-
-        System.out.println("url ==> ");
-        System.out.println(Is.url("http://sh.lianjia.com/")); //true
-        System.out.println(Is.url("https://sh.lianjia.com/")); //true
-        System.out.println(Is.url("ftp://192.168.1.1")); //true
-    }
+    private static final Pattern MOBILE = Pattern.compile("^(01|1)\\d{10}$");
+    private static final Pattern IP4 = Pattern.compile("^(?:(?:\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])\\.){3}(?:\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])$");
+    private static final Pattern IP6 = Pattern.compile("^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\\-]*[a-zA-Z0-9])\\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\\-]*[A-Za-z0-9])$|^\\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}))|:)))(%.+)?\\s*$");
 }
---
+++ src/test/java/com/dooioo/td/utils/TestIs.java
@@ -0,0 +1,118 @@
+package com.dooioo.td.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.*;
+
+/**
+ * Created by chopperkuang on 15/12/7.
+ *
+ * @author 闷骚乔巴
+ * @since 15/12/7
+ */
+@RunWith(JUnit4.class)
+public class TestIs {
+
+    @Test
+    public void testStringEmpty() {
+
+    }
+
+    @Test
+    public void testObjectEmpty() {
+
+        String nullStr = null;
+        String emptyStr = "";
+        String emptyAndSpaceStr = " ";
+        String notEmpty = "cc";
+
+        Assert.assertTrue(Is.empty(nullStr));
+        Assert.assertTrue(Is.empty(emptyStr));
+        Assert.assertTrue(Is.empty(emptyAndSpaceStr));
+        Assert.assertFalse(Is.empty(notEmpty));
+
+
+        Map<String, String> emptyMap = new HashMap<>();
+        Map<String, String> notEmptyMap = new HashMap<String, String>(){{
+            put("test", "ok");
+        }};
+
+        Assert.assertTrue(Is.empty(emptyMap));
+        Assert.assertFalse(Is.empty(notEmptyMap));
+
+        List nullList = null;
+        List<String> emptyList = new ArrayList<>();
+        List<String> notEmptyList = Arrays.asList("cc");
+
+        Assert.assertTrue(Is.empty(emptyList));
+        Assert.assertTrue(Is.empty(nullList));
+        Assert.assertFalse(Is.empty(notEmptyList));
+
+        String [] emptys = null;
+        String [] notEmptys = new String[]{"1", "2"};
+
+        Assert.assertTrue(Is.empty(emptys));
+        Assert.assertFalse(Is.empty(notEmptys));
+    }
+
+    @Test
+    public void testUrl() {
+        Assert.assertTrue(Is.url("http://sh.lianjia.com/"));
+        Assert.assertTrue(Is.url("https://sh.lianjia.com/"));
+        Assert.assertTrue(Is.url("http://sh.lianjia.com"));
+        Assert.assertTrue(Is.url("http://sh.lianjia.com:8080"));
+        Assert.assertTrue(Is.url("ftp://sh.lianjia.com"));
+
+        Assert.assertFalse(Is.url("ftp://192.168.1.1"));
+        Assert.assertFalse(Is.url("http://localhost:8080"));
+        Assert.assertFalse(Is.url("http://127.0.0.1:8080"));
+        Assert.assertFalse(Is.url(""));
+    }
+
+    @Test
+    public void testEmail() {
+        Assert.assertTrue(Is.email("Chopperkuang@gmail.com"));
+        Assert.assertTrue(Is.email("chopperkuang@gmail.com"));
+        Assert.assertTrue(Is.email("中国@gmail.com"));
+
+        Assert.assertFalse(Is.email("chopperkuanggmail.com"));
+        Assert.assertFalse(Is.email("ch"));
+        Assert.assertFalse(Is.email(""));
+    }
+
+    @Test
+    public void testMobile() {
+        Assert.assertTrue(Is.mobile("15601622811"));
+        Assert.assertTrue(Is.mobile("13512341234"));
+        Assert.assertTrue(Is.mobile("013512341234"));
+        Assert.assertTrue(Is.mobile("015601622811"));
+        Assert.assertTrue(Is.mobile("0156 0162 2811"));
+        Assert.assertTrue(Is.mobile("0 1560 1622 811"));
+        Assert.assertTrue(Is.mobile("08615601622811"));
+        Assert.assertTrue(Is.mobile("8615601622811"));
+        Assert.assertTrue(Is.mobile("+8615601622811"));
+
+        Assert.assertFalse(Is.mobile("25601622811"));
+        Assert.assertFalse(Is.mobile("256016228111"));
+        Assert.assertFalse(Is.mobile("2560162281111"));
+        Assert.assertFalse(Is.mobile("6370123"));
+        Assert.assertFalse(Is.mobile("2"));
+    }
+
+    @Test
+    public void testIP() {
+        Assert.assertTrue(Is.ip("192.168.1.1")); //ipv4
+        Assert.assertTrue(Is.ip("255.255.1.1")); //ipv4
+        Assert.assertTrue(Is.ip("127.0.0.1")); //ipv4
+        Assert.assertTrue(Is.ip("123.123.123.123")); //ipv4
+        Assert.assertTrue(Is.ip("2001:DB8:0:0:1::1")); //ipv6
+        Assert.assertTrue(Is.ip("1:50:198:2::1:2:8")); //ipv6
+        Assert.assertTrue(Is.ip("2000::1:2345:6789:abcd")); //ipv6
+
+        Assert.assertFalse(Is.ip("1.2.3"));
+
+    }
+}
